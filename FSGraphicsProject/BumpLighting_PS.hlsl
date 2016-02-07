3989f52546f905eb56d13122932f68c0700ba77e
Shiyang Ao <aosyang@hotmail.com>
2016-02-06 19:59:10 -0500

    Pack lighting shaders into header file.

--- FSGraphicsProject/BumpLighting_PS.hlsl
+++ FSGraphicsProject/BumpLighting_PS.hlsl
@@ -5,6 +5,7 @@
 //=============================================================================
 
 #include "ConstBufferPS.h"
+#include "LightShaderCommon.hlsli"
 
 Texture2D DiffuseTexture	: register(t0);
 Texture2D NormalTexture		: register(t1);
@@ -45,57 +46,42 @@ float4 main(OUTPUT_VERTEX Input) : SV_TARGET
 
 	for (int id = 0; id < DirectionalLightCount; id++)
 	{
-		float3 lightDir = DirectionalLight[id].Direction.xyz;
-
 		// Diffuse lighting
-		float DiffuseIntensity = dot(normal, lightDir);
-		Diffuse.rgb += saturate(DiffuseIntensity) * DirectionalLight[id].Color.rgb * DirectionalLight[id].Color.w;
-		
+		Diffuse.rgb += CalculateDiffuseLight(normal, DirectionalLight[id].Direction.xyz, DirectionalLight[id].Color);
+
 		// Specular lighting
-		float3 halfVec = normalize(lightDir + viewDir);
-		float SpecularIntensity = max(pow(saturate(dot(normal, normalize(halfVec))), SpecularColorAndPower.w), 0.0f);
-		Specular.rgb += DirectionalLight[id].Color.rgb * SpecularColorAndPower.rgb * SpecularIntensity;
+		Specular.rgb += CalculateSpecularLight(normal, DirectionalLight[id].Direction.xyz, viewDir, DirectionalLight[id].Color, SpecularColorAndPower);
 	}
 
 	for (int ip = 0; ip < PointLightCount; ip++)
 	{
-		// Diffuse lighting
 		float3 lightVec = PointLight[ip].PosAndRadius.xyz - Input.PosW;
 		float3 lightDir = normalize(lightVec);
 
 		float attenuation = 1.0f - saturate(length(lightVec) / PointLight[ip].PosAndRadius.w);
-		float DiffuseIntensity = dot(normal, lightDir);
 
-		Diffuse.rgb += saturate(DiffuseIntensity * attenuation) * PointLight[ip].Color.rgb * PointLight[ip].Color.w;
+		// Diffuse lighting
+		Diffuse.rgb += attenuation * CalculateDiffuseLight(normal, lightDir, PointLight[ip].Color);
 
 		// Specular lighting
-		float3 halfVec = normalize(lightDir + viewDir);
-		float SpecularIntensity = max(pow(saturate(dot(normal, normalize(halfVec))), SpecularColorAndPower.w), 0.0f);
-		Specular.rgb += attenuation * PointLight[ip].Color.rgb * SpecularColorAndPower.rgb * SpecularIntensity;
+		Specular.rgb += attenuation * CalculateSpecularLight(normal, lightDir, viewDir, PointLight[ip].Color, SpecularColorAndPower);
 	}
 
 	for (int is = 0; is < SpotlightCount; is++)
 	{
-		// Diffuse lighting
 		float3 lightVec = Spotlight[is].PosAndInnerConeRatio.xyz - Input.PosW;
 		float3 lightDir = normalize(lightVec);
-		float3 coneDir = Spotlight[is].ConeDirAndOuterConeRatio.xyz;
 
 		float surfaceRatio = saturate(dot(-lightDir, Spotlight[is].ConeDirAndOuterConeRatio.xyz));
-		// spotFactor = (surfaceRatio > Spotlight[is].ConeDirAndRatio.w) ? 1.0f : 0.0f;
-
 		float attenuation = 1.0f - saturate((Spotlight[is].PosAndInnerConeRatio.w - surfaceRatio) / (Spotlight[is].PosAndInnerConeRatio.w - Spotlight[is].ConeDirAndOuterConeRatio.w));
 
-		float DiffuseIntensity = dot(normal, lightDir);
-		Diffuse.rgb += attenuation * saturate(DiffuseIntensity) * Spotlight[is].Color.rgb * Spotlight[is].Color.w;
+		// Diffuse lighting
+		Diffuse.rgb += attenuation * CalculateDiffuseLight(normal, lightDir, Spotlight[is].Color);
 
 		// Specular lighting
-		float3 halfVec = normalize(lightDir + viewDir);
-		float SpecularIntensity = max(pow(saturate(dot(normal, normalize(halfVec))), SpecularColorAndPower.w), 0.0f);
-		Specular.rgb += attenuation * Spotlight[is].Color.rgb * SpecularColorAndPower.rgb * SpecularIntensity;
+		Specular.rgb += attenuation * CalculateSpecularLight(normal, lightDir, viewDir, Spotlight[is].Color, SpecularColorAndPower);
 	}
 
-	//Diffuse.rgb = saturate(Diffuse.rgb);
 	Diffuse.a = 1.0f;
 
 	return Diffuse * DiffuseTexture.Sample(Sampler, Input.UV) + Specular;
---
+++ FSGraphicsProject/LightShaderCommon.hlsli
@@ -0,0 +1,29 @@
+//=============================================================================
+// LightShaderCommon.hlsli by Shiyang Ao, 2016 All Rights Reserved.
+//
+// 
+//=============================================================================
+
+#ifndef _LIGHTSHADERCOMMON_HLSLI
+#define _LIGHTSHADERCOMMON_HLSLI
+
+float3 CalculateDiffuseLight(float3 normal,
+							 float3 lightDir,
+							 float4 lightColor)
+{
+	float DiffuseIntensity = dot(normal, lightDir);
+	return saturate(DiffuseIntensity) * lightColor.rgb * lightColor.a;
+}
+
+float3 CalculateSpecularLight(float3 normal,
+							  float3 lightDir,
+							  float3 viewDir,
+							  float4 lightColor,
+							  float4 specularColor)
+{
+	float3 halfVec = normalize(lightDir + viewDir);
+	float SpecularIntensity = max(pow(saturate(dot(normal, normalize(halfVec))), specularColor.w), 0.0f);
+	return lightColor.rgb * specularColor.rgb * SpecularIntensity;
+}
+
+#endif
