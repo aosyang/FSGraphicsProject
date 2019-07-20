26351639272b0f7fe5bc28904f0744ab0ab1b40d
Shiyang Ao <aosyang@gmail.com>
2019-07-20 02:53:42 -0700

    Added debug variables for debugging shadow maps.

--- RhinoEngine/RenderSystem/RDirectionalLightComponent.cpp
+++ RhinoEngine/RenderSystem/RDirectionalLightComponent.cpp
@@ -15,7 +15,13 @@ RMatrix4(
 	0.0f, 0.0f, 1.0f, 0.0f,
 	0.5f, 0.5f, 0.0f, 1.0f);
 
-float shadowSplitPoints[4] = { 0.0f, 0.05f, 0.4f, 1.0f };
+namespace
+{
+	float shadowSplitPoints[4] = { 0.0f, 0.05f, 0.4f, 1.0f };
+
+	bool DrawDebugCascadedFrustum = false;
+	bool DebugFreezeFrustum = false;
+}
 
 RDirectionalLightComponent::RDirectionalLightComponent(RSceneObject* InOwner)
 	: Base(InOwner),
@@ -55,31 +61,60 @@ bool RDirectionalLightComponent::CanCastShadow()
 
 void RDirectionalLightComponent::PrepareDepthPass(int PassIndex, const RenderViewInfo& View)
 {
+	assert(PassIndex >= 0 && PassIndex < GetDepthPassesNum());
+	assert(View.Frustum);
+
 	auto& cbScene = RConstantBuffers::cbScene.Data;
 	RConstantBuffers::cbScene.ClearData();
 
-	float lightDistance[3] = { 1000.0f, 2000.0f, 2000.0f };
-	RColor frustumColor[3] = { RColor(1, 0, 0), RColor(0, 1, 0), RColor(0, 0, 1) };
+	// Distance between shadow caster and center of frustum
+	float LightDistPerLevel[3] = { 1000.0f, 2000.0f, 2000.0f };
 
-	const RFrustum& frustum = *(View.Frustum);
+	RFrustum* ViewFrustum = View.Frustum;
 
-	RSphere s0 = CalculateBoundingSphereFromFrustum(frustum, shadowSplitPoints[PassIndex], shadowSplitPoints[PassIndex + 1]);
+	// Freeze view frustum for debugging
+	{
+		static bool LastDebugState = false;
+		if (DebugFreezeFrustum)
+		{
+			static RFrustum DebugFrustum;
 
-	//if (freezeFrustum)
-	//	GDebugRenderer.DrawSphere(s0.center, s0.radius, frustumColor[PassIndex]);
+			// Update debug frustum every time when debug freeze frustum is enabled
+			if (DebugFreezeFrustum != LastDebugState)
+			{
+				DebugFrustum = *View.Frustum;
+			}
 
-	lightDistance[PassIndex] = max(lightDistance[PassIndex], s0.radius);
+			ViewFrustum = &DebugFrustum;
+		}
 
-	RVec3 shadowTarget = s0.center;
-	RVec3 shadowEyePos = shadowTarget + m_LightDirection * lightDistance[PassIndex];
+		LastDebugState = DebugFreezeFrustum;
+	}
+
+	// A minimal bounding sphere that covers one slice of the view frustum
+	RSphere BoundSphere = CalculateBoundingSphereFromFrustum(*ViewFrustum, shadowSplitPoints[PassIndex], shadowSplitPoints[PassIndex + 1]);
+
+	// Color for debug drawing each level of view frustum
+	static RColor FrustumDebugColor[3] = { RColor(1, 0, 0), RColor(0, 1, 0), RColor(0, 0, 1) };
+
+	if (DrawDebugCascadedFrustum)
+	{
+		GDebugRenderer.DrawSphere(BoundSphere.center, BoundSphere.radius, FrustumDebugColor[PassIndex]);
+	}
+
+	// Extend the distance to the light by the bounding sphere's size
+	//LightDistPerLevel[PassIndex] = max(LightDistPerLevel[PassIndex], BoundSphere.radius);
+
+	RVec3 shadowTarget = BoundSphere.center;
+	RVec3 shadowEyePos = shadowTarget + m_LightDirection * LightDistPerLevel[PassIndex];
 
 	RVec3 viewForward = (shadowTarget - shadowEyePos).GetNormalized();
 	RVec3 viewRight = RVec3::Cross(RVec3(0, 1, 0), viewForward).GetNormalized();
 	RVec3 viewUp = RVec3::Cross(viewForward, viewRight).GetNormalized();
 
 	// Calculate texel offset in world space
-	float texel_unit = s0.radius * 2.0f / 1024.0f;
-	float texel_depth_unit = (s0.radius + lightDistance[PassIndex]) / 1024.0f;
+	float texel_unit = BoundSphere.radius * 2.0f / 1024.0f;
+	float texel_depth_unit = (BoundSphere.radius + LightDistPerLevel[PassIndex]) / 1024.0f;
 
 	float dx = RVec3::Dot(shadowEyePos, viewRight);
 	float dy = RVec3::Dot(shadowEyePos, viewUp);
@@ -95,12 +130,14 @@ void RDirectionalLightComponent::PrepareDepthPass(int PassIndex, const RenderVie
 
 	RFrustum shadowVolume = m_ShadowMap[PassIndex].GetFrustum();
 
-	//if (freezeFrustum)
-	//	GDebugRenderer.DrawFrustum(shadowVolume, frustumColor[PassIndex]);
+	if (DrawDebugCascadedFrustum)
+	{
+		GDebugRenderer.DrawFrustum(shadowVolume, FrustumDebugColor[PassIndex]);
+	}
 
 	m_ShadowMap[PassIndex].SetViewMatrix(shadowViewMatrix);
 	//m_ShadowMap.SetOrthogonalProjection(500.0f, 500.0f, 0.1f, 5000.0f);
-	m_ShadowMap[PassIndex].SetOrthogonalProjection(s0.radius * 2.0f, s0.radius * 2.0f, 0.1f, s0.radius + lightDistance[PassIndex]);
+	m_ShadowMap[PassIndex].SetOrthogonalProjection(BoundSphere.radius * 2.0f, BoundSphere.radius * 2.0f, 0.1f, BoundSphere.radius + LightDistPerLevel[PassIndex]);
 
 	RMatrix4 shadowViewProjMatrix = m_ShadowMap[PassIndex].GetViewMatrix() * m_ShadowMap[PassIndex].GetProjectionMatrix();
 	cbScene.shadowViewProjMatrix[PassIndex] = shadowViewProjMatrix;
@@ -134,7 +171,7 @@ void RDirectionalLightComponent::PrepareRenderPass()
 int RDirectionalLightComponent::GetDepthPassesNum() const
 {
 	// Three-pass cascaded shadow map
-	return 3;
+	return MAX_CASCADED_SHADOW_SPLITS_NUM;
 }
 
 RVec4 RDirectionalLightComponent::GetShadowDepth(RCamera* ViewCamera)
@@ -197,11 +234,16 @@ RSphere RDirectionalLightComponent::CalculateBoundingSphereFromFrustum(const RFr
 	RVec3 center = (farMidPoint + nearMidPoint) * 0.5f;
 	RSphere s = { center, (cornerPoints[4] - center).Magnitude() };
 
-	//GDebugRenderer.DrawSphere(center, 50.0f, RColor(1, 0, 0));
-
-	//for (int i = 4; i < 8; i++)
+	//if (DrawDebugCascadedFrustum)
 	//{
-	//	GDebugRenderer.DrawSphere(cornerPoints[i], 50.0f, RColor(1, 0, 0));
+	//	// Center point of the frustum
+	//	GDebugRenderer.DrawSphere(center, 50.0f, RColor(1, 1, 0), 8);
+
+	//	// Corner points of the frustum
+	//	for (int i = 4; i < 8; i++)
+	//	{
+	//		GDebugRenderer.DrawSphere(cornerPoints[i], 50.0f, RColor(1, 0, 0), 8);
+	//	}
 	//}
 
 	return s;
