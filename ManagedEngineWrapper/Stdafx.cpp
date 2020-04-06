47447c5084ace91b8f62fc24441b4b3a764d2fb5
Shiyang Ao <aosyang@gmail.com>
2020-04-05 17:36:47 -0700

    Removed C# level editor
    * The ImGui editor is better

--- ManagedEngineWrapper/Stdafx.cpp
+++
@@ -1,9 +0,0 @@
-//=============================================================================
-// Shiyang Ao, 2018 All Rights Reserved.
-//
-// stdafx.cpp : source file that includes just the standard includes
-// EngineManagedWrapper.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-//=============================================================================
-
-#include "stdafx.h"
